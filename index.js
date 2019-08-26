e6aebbb5016ba6862fdad04fcfd260c6937d46f6
jdburkholder <jonahdburkholder@gmail.com>
2019-08-25 22:07:33 -0400

    bot login works!

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const Discord = require('discord.js')
-const client = new Discord.Client 
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const auth = require('./auth.json');
+const token = auth.token;
+
+bot.on('ready', () =>{
+    console.log('online');
+})
+
+bot.login(token);
