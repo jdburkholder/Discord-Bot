f614e44e60b1f6115582c8e2faa3f52e82f2b6ea
jdburkholder <jonahdburkholder@gmail.com>
2019-09-15 14:29:28 -0400

    added commands and async

--- index.js
+++ index.js
@@ -10,14 +10,27 @@ bot.on('ready', () =>{
     console.log('online');
 })
 
-bot.on('message', message =>{
-    
+bot.on('message', async message =>{
+
+    if (message.author.bot) return;
+
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
+        default:
+            message.reply("I don't understand. Try m!help.");
+        break;
         case 'help':
             message.reply("Hi! I don't have any commands right now.");
         break;
+        case 'status':
+            message.reply("I don't know how to tell yet.");
+        break;
+        case 'start':
+            message.reply("Starting! (not really)");
+        break;
+        case 'shutdown':
+            message.reply("Shutting down. (not really)");
     }
 })
 
